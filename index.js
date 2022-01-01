5f70849e69bb2f626a744b6339c3e21ed690c02a
tabbycat01 <geraldywy@gmail.com>
2022-01-02 01:14:24 +0800

    add some media query

--- index.js
+++ index.js
@@ -80,8 +80,48 @@ let comments = [
         content: "I LOVE HER",
         likeCount: 2,
     },
-
-
+    {
+        authorPic: "assets/nah.jpeg",
+        authorName: "nah",
+        age: "5 months ago",
+        content: "she's so talented",
+        likeCount: 22,
+    },
+    {
+        authorPic: "assets/LeBeautiful.jpeg",
+        authorName: "LeBeautiful",
+        age: "3 months ago",
+        content: "glad to be a fan now, she's definitely gonna go places! 나는 팬이다!",
+        likeCount: 6,
+    },
+    {
+        authorPic: "assets/紙短情長.jpeg",
+        authorName: "紙短情長",
+        age: "2 months ago",
+        content: "It’s gonna lit I swear 🤤❤️‍🔥❤️‍🔥",
+        likeCount: 1,
+    },
+    {
+        authorPic: "assets/Last KiNG.jpeg",
+        authorName: "Last KiNG",
+        age: "2 months ago",
+        content: "OSSHite! Fake is my fave on this playlist 🔥🔥",
+        likeCount: 1,
+    },
+    {
+        authorPic: "assets/Gray Syu.jpeg",
+        authorName: "Gray Syu",
+        age: "4 months ago",
+        content: "她的聲音真的很好聽",
+        likeCount: 7,
+    },
+    {
+        authorPic: "assets/sarah lol.jpeg",
+        authorName: "sarah lol",
+        age: "4 months ago",
+        content: "I LOVE HER",
+        likeCount: 2,
+    },
 ]
 
 let commentSection = document.querySelector(".comment-section")
@@ -90,3 +130,4 @@ for (let i in comments) {
     let comment = createNewComment(comments[i])
     commentSection.insertAdjacentHTML("beforeend", comment)
 }
+
